02134ffe184517f1e9afbf8fd0aeee11fc916a7e
rithbmc <chhimsokrith@psis.edu.kh>
2024-08-15 21:28:24 +0700

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import './assets/tailwind.css';
 import router from './router';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 createApp(App).use(router).mount('#app')
