8407d490618085bff4f53d41c2dd0fd05387ad90
rithbmc <chhimsokrith@psis.edu.kh>
2024-08-16 18:33:24 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../Views/Home.vue';
-import About from '../views/About.vue';
 import Contact from '../views/Contact.vue';
 import EarnMoney from '../Views/EarnMoney.vue';
 import Wallet from '../Views/Wallet.vue';
@@ -25,7 +24,7 @@ const routes = [
   { 
     path: '/about', 
     name: 'about',
-    component: About 
+    component: () => import("../views/About.vue") 
   },
   { 
     path: '/contact', 
